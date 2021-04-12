42740a1a74b70382c6f74910762abd947ba32d53
Rafael Rollo <rafaelrollo92@gmail.com>
2021-04-11 23:45:23 -0300

    Initial commit
    project generated by spring initializr

---
+++ src/main/java/br/com/rollo/rafael/casadocodigoapi/CasadocodigoApiApplication.java
@@ -0,0 +1,11 @@
+package br.com.rollo.rafael.casadocodigoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasadocodigoApiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CasadocodigoApiApplication.class, args);
+	}
+}
